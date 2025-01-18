PathOfBuildingCommunity/PathOfBuilding

commit 6ec330f0b0ec44cbf7489541a244c49ae8431f8b

FIX: formless inferno not increasing miniion life. (#7370)

src/Modules/CalcDefence.lua
@@ -438,6 +438,11 @@ function calcs.defence(env, actor)
 		end
 	end
 
+	if actor == env.minion then
+		doActorLifeMana(env.minion)
+		doActorLifeManaReservation(env.minion)
+	end
+
 	-- Block
 	output.BlockChanceMax = modDB:Sum("BASE", nil, "BlockChanceMax")
 	if modDB:Flag(nil, "MaximumBlockAttackChanceIsEqualToParent") then

src/Modules/CalcPerform.lua
@@ -57,7 +57,7 @@ local function mergeKeystones(env)
 	end
 end
 
-local function doActorLifeMana(actor)
+function doActorLifeMana(actor)
 	local modDB = actor.modDB
 	local output = actor.output
 	local breakdown = actor.breakdown
@@ -437,7 +437,7 @@ end
 
 -- Calculate life/mana reservation
 ---@param actor table
-local function doActorLifeManaReservation(actor)
+function doActorLifeManaReservation(actor)
 	local modDB = actor.modDB
 	local output = actor.output
 	local condList = modDB.conditions
@@ -3015,9 +3015,6 @@ function calcs.perform(env, fullDPSSkipEHP)
 	end
 
 	if env.minion then
-		doActorLifeMana(env.minion)
-		doActorLifeManaReservation(env.minion)
-
 		calcs.defence(env, env.minion)
 		if not fullDPSSkipEHP then -- main.build.calcsTab.input.showMinion and -- should be disabled unless "calcsTab.input.showMinion" is true
 			calcs.buildDefenceEstimations(env, env.minion)

bct8925/PathOfBuilding-PoE2

commit 711199f81c856d99de700a7f3601ee9847368a1e

FIX: Breakdown tbl null deref
Various breakdown tables were being used by the call to
doActorLifeManaSpirit way before being created.

Likely caused by https://github.com/PathOfBuildingCommunity/PathOfBuilding/pull/7370

src/Modules/CalcDefence.lua
@@ -702,6 +702,15 @@ function calcs.defence(env, actor)
 		end
 	end
 
+	if breakdown then
+		breakdown.Ward = { slots = { } }
+		breakdown.EnergyShield = { slots = { } }
+		breakdown.Armour = { slots = { } }
+		breakdown.Evasion = { slots = { } }
+		breakdown.Life = { slots = { } }
+		breakdown.Mana = { slots = { } }
+		breakdown.Spirit = { slots = { } }
+	end
 	if actor == env.minion then
 		calcs.doActorLifeManaSpirit(env.minion)
 		calcs.doActorLifeManaSpiritReservation(env.minion)
@@ -847,15 +856,6 @@ function calcs.defence(env, actor)
 		local energyShield = 0
 		local armour = 0
 		local evasion = 0
-		if breakdown then
-			breakdown.Ward = { slots = { } }
-			breakdown.EnergyShield = { slots = { } }
-			breakdown.Armour = { slots = { } }
-			breakdown.Evasion = { slots = { } }
-			breakdown.Life = { slots = { } }
-			breakdown.Mana = { slots = { } }
-			breakdown.Spirit = { slots = { } }
-		end
 		local energyShieldBase, armourBase, evasionBase, wardBase
 		local gearWard = 0
 		local gearEnergyShield = 0