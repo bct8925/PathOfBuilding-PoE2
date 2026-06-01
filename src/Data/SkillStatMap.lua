634bcc7c662c3c61dedc1950ae251ebca48cf879
TarikBeentjes61 <50704992+TarikBeentjes61@users.noreply.github.com>
2026-06-01 08:34:55 +0200

    Add support for Rapid Casting III (#1965)

--- src/Data/SkillStatMap.lua
+++ src/Data/SkillStatMap.lua
@@ -617,6 +617,9 @@ return {
 ["grenade_skill_cooldown_speed_+%"] = {
 	mod("CooldownRecovery", "INC", nil),
 },
+["cast_speed_+%_per_num_unique_spells_cast_in_last_8_seconds"] = {
+	mod("Speed", "INC", nil, ModFlag.Cast, 0, { type = "Multiplier", var = "NonInstantSpellCastRecently" }),
+},
 -- AoE
 ["active_skill_base_area_of_effect_radius"] = {
 	skill("radius", nil),
